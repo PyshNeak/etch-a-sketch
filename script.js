b32223af1d12b4d8b400d855730f6e804f0d8083
Max Pyshniak <maxpyshniak@gmail.com>
2024-11-27 08:59:20 -0700

    Create 16x16 grid

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+// Create 16x16 grid
+const container = document.querySelector('.container');
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement('div');
+    row.classList.add('grid-row')
+    for (let j = 0; j < 16; j++) {
+        const square = document.createElement('div');
+        square.classList.add('grid-square');
+        row.appendChild(square);
+    }
+    container.appendChild(row);
+}
