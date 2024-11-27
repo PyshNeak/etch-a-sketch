965c5d6873dbe4933bb2ffc986ec471dbac06d42
Max Pyshniak <maxpyshniak@gmail.com>
2024-11-27 12:04:03 -0700

    Add additional coloring options

--- script.js
+++ script.js
@@ -1,3 +1,78 @@
+let isToggle, isRandom, isDarkening;
+isToggle = isRandom = isDarkening = false;
+
+function toggle(e) {
+    e.target.style.background = 'rgba(0, 0, 0, 0.1)';
+}
+
+function random(e) {
+    let c1 = Math.round(Math.random() * 255)
+    let c2 = Math.round(Math.random() * 255)
+    let c3 = Math.round(Math.random() * 255)
+    e.target.style.background = `rgba(${c1}, ${c2}, ${c3}, 0.1)`
+}
+
+function darkening(e) {
+    let orig = e.target.style.background;
+    if (orig) {
+        let opacity = +orig.slice(orig.length - 4, orig.length - 1);
+        if (opacity < 1) {
+            opacity += 0.1;
+        }
+
+        e.target.style.background = orig.slice(0, orig.length - 4) + opacity + ')';
+    }
+}
+
+function setGridFunctionality(functionality='toggle') {
+    console.log(functionality)
+    const squares = document.querySelectorAll('.grid-square');
+    let func;
+    removeGridFunctionality();
+    switch (functionality) {
+        case 'toggle':
+            isToggle = true;
+            func = toggle;
+            //squares.forEach((square) => {
+            //    square.addEventListener('mouseover', toggle)
+            //});
+            break
+        case 'random':
+            isRandom = true;
+            func = random;
+            //squares.forEach((square) => {
+            //    square.addEventListener('mouseover', random)
+            //});
+            break
+        case 'darkening':
+            isDarkening = true;
+            func = darkening;
+            //squares.forEach((square) => {
+            //    square.addEventListener('mouseover', darkening)
+            //});
+            break
+    }
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', func);
+    });
+}
+
+function removeGridFunctionality() {
+    const squares = document.querySelectorAll('.grid-square');
+    let func;
+    if (isToggle) {
+        func = toggle;
+    } else if (isRandom) {
+        func = random;
+    } else if (isDarkening) {
+        func = darkening;
+    }
+    squares.forEach((square) => {
+        square.removeEventListener('mouseover', func);
+    })
+    isToggle = isRandom = isDarkening = false;
+}
+
 function createGrid(size = 16) {
     const container = document.querySelector('.container');
 
@@ -13,23 +88,15 @@ function createGrid(size = 16) {
         container.appendChild(row);
     }
 
-    // Set squares to highlight when passed over by mouse
-    const squares = document.querySelectorAll('.grid-square');
-    squares.forEach((square) => {
-        square.addEventListener('mouseover', (e) => {
-            e.target.classList.toggle('highlight');
-        });
-    });
+    // Set squares highlight functionality
+    setGridFunctionality();
 }
 createGrid();
 
 // Clears and creates new grid of specified size
 function updateGrid(size) {
     const container = document.querySelector('.container');
-    let curNode = document.querySelector('button');
-    while (curNode.nextSibling) {
-        container.removeChild(curNode.nextSibling);
-    }
+    container.textContent = "";
     createGrid(size);
 }
 
@@ -38,4 +105,12 @@ const btn = document.querySelector('button');
 btn.addEventListener('click', (e) => {
     let size = prompt('Enter new grid size: ');
     updateGrid(size);
+});
+
+// Add change event listener to radio buttons
+const radios = document.querySelectorAll('input[name="update-modifier"]')
+radios.forEach((radio) => {
+    radio.addEventListener('change', (e) => {
+        setGridFunctionality(e.target.value);
+    });
 });
